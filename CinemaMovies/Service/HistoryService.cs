11b02b0581b709da4bbafd4f0a10d6d836cc1b5b
TRTL <4seven@gmail.com>
2022-12-29 21:39:50 +0200

    history service, invoice service, dtos

--- CinemaMovies/Service/HistoryService.cs
+++ CinemaMovies/Service/HistoryService.cs
@@ -1,6 +1,32 @@
-﻿namespace CinemaMovies.Service
+﻿using CinemaMovies.Models;
+using CinemaMovies.Models.DTO;
+using CinemaMovies.Service.IService;
+
+namespace CinemaMovies.Service
 {
-    public class HistoryService
+    public class HistoryService : IHistoryService
     {
+        public PurchaseHistoryDto BuildPurchaseHistory(List<Purchase> purchases)
+        {
+            var newPurchaseHistoryDto = new PurchaseHistoryDto()
+            {
+                Name = "",
+                Purchases = new List<PurchaseDto>()
+            };
+
+            foreach (var purchase in purchases)
+            {
+                if (newPurchaseHistoryDto.Name == "") newPurchaseHistoryDto.Name = purchase.User.UserName;
+
+                var newPurchaseDto = new PurchaseDto()
+                {
+                    PurchaseId = purchase.PurchaseId,
+                    Date = purchase.DateTime,
+                    TotalPrice = purchase.TotalPrice
+                };
+                newPurchaseHistoryDto.Purchases.Add(newPurchaseDto);
+            }
+            return newPurchaseHistoryDto;
+        }
     }
 }
--- CinemaMovies/Service/InvoiceService.cs
+++ CinemaMovies/Service/InvoiceService.cs
@@ -1,6 +1,14 @@
-﻿namespace CinemaMovies.Service
+﻿using CinemaMovies.Models;
+using CinemaMovies.Models.DTO;
+using CinemaMovies.Service.IService;
+
+namespace CinemaMovies.Service
 {
-    public class InvoiceService
+    public class InvoiceService : IInvoiceService
     {
+        public InvoiceDto BuildInvoice(Purchase purchase)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
